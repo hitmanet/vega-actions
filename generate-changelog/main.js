22003764eabcd34bd15ad72f4cf1d487ee20191b
hitmanet <nathan200033@gmail.com>
2020-06-08 14:35:25 +0300

    fear(actions): генерация логов

---
+++ generate-changelog/main.js
@@ -0,0 +1,15 @@
+
+const cp = require("child_process");
+
+const path = require("path");
+const core = require("@actions/core");
+const lernaChangelog = path.resolve(__dirname, "node_modules/@hitmanet808/lerna-changelog");
+
+const exec = cmd => cp.execSync(cmd).toString().trim();
+
+const tagFrom = core.getInput("from", { required: true });
+const tagTo = core.getInput("to", { required: true });
+
+const changelog = exec(`node ${lernaChangelog} --from ${tagFrom} --to ${tagTo}`);
+
+core.setOutput("changelog", JSON.stringify(changelog));
